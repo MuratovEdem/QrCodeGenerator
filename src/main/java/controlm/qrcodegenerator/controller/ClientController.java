e661c0b93eaf6126596fed16a418d3dd5bc3475c
Edem <edem.muratov01@mail.ru>
2026-01-18 11:21:43 +0300

    123

--- src/main/java/controlm/qrcodegenerator/controller/ClientController.java
+++ src/main/java/controlm/qrcodegenerator/controller/ClientController.java
@@ -1,7 +1,9 @@
 package controlm.qrcodegenerator.controller;
 
+import controlm.qrcodegenerator.dto.request.ProtocolRequestDto;
 import controlm.qrcodegenerator.model.Client;
 import controlm.qrcodegenerator.service.ClientService;
+import controlm.qrcodegenerator.service.ProtocolService;
 import controlm.qrcodegenerator.service.QRCodeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
@@ -9,8 +11,10 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -23,6 +27,7 @@ public class ClientController {
 
     private final ClientService clientService;
     private final QRCodeService qrCodeService;
+    private final ProtocolService protocolService;
 
     @GetMapping
     public String listClients(Model model) {
@@ -41,6 +46,14 @@ public class ClientController {
         Client client = clientService.getClientById(id);
         model.addAttribute("client", client);
         return "public/client-view";
+    }
+
+    @PostMapping("/{id}/create-protocol")
+    public String createProtocolByClientId(@PathVariable Long id, BindingResult result) {
+//        ProtocolRequestDto protocolRequestDto = new ProtocolRequestDto();
+//        protocolRequestDto.setName(result.getObjectName());
+//        protocolService.createProtocolByClientId(result, id);
+        return "protocols/create-protocol-form";
     }
 
     @GetMapping("/{id}/qr")
