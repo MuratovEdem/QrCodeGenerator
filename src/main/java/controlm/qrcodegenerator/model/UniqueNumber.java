00a34f79d4c564398c1222857b46d69a071000e6
Edem <edem.muratov01@mail.ru>
2026-03-12 12:28:00 +0300

    12 03 2026 commit

--- src/main/java/controlm/qrcodegenerator/model/UniqueNumber.java
+++ src/main/java/controlm/qrcodegenerator/model/UniqueNumber.java
@@ -1,13 +1,6 @@
 package controlm.qrcodegenerator.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -23,6 +16,7 @@ public class UniqueNumber {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private Long number;
 
     @ManyToOne(fetch = FetchType.LAZY)
